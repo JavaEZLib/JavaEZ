41fc9583fd5e15fc6ad5b2cc024d06cdf8b38545
RedstoneBoy0509 <jacob.rutledge0509@gmail.com>
2021-05-25 11:23:53 -0500

    Amend Update 1.2

--- src/main/java/io/github/redstoneboy0509/javaez/JavaEZ.java
+++ src/main/java/io/github/redstoneboy0509/javaez/JavaEZ.java
@@ -17,7 +17,7 @@ public class JavaEZ {
      * The current version of JavaEZ.
      * @since 1.1
      */
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     /**
      * Prints info about your version of JavaEZ
